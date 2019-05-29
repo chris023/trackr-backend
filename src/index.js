5aea7015e43686c8f24755eaa6db5613f0a29ff2
chris023 <chris.meyer023@gmail.com>
2019-05-29 17:48:45 -0600

    Add introspection and playground to server config

--- src/index.js
+++ src/index.js
@@ -1,38 +1,35 @@
-import 'dotenv/config';
-import cors from 'cors';
-import http from 'http';
-import jwt from 'jsonwebtoken';
-import DataLoader from 'dataloader';
-import express from 'express';
-import {
-  ApolloServer,
-  AuthenticationError,
-} from 'apollo-server-express';
+import 'dotenv/config'
+import cors from 'cors'
+import http from 'http'
+import jwt from 'jsonwebtoken'
+import DataLoader from 'dataloader'
+import express from 'express'
+import { ApolloServer, AuthenticationError } from 'apollo-server-express'
 
-import schema from './schema';
-import resolvers from './resolvers';
-import models, { sequelize } from './models';
-import loaders from './loaders';
+import schema from './schema'
+import resolvers from './resolvers'
+import models, { sequelize } from './models'
+import loaders from './loaders'
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
 const getMe = async req => {
-  const token = req.headers['x-token'];
+  const token = req.headers['x-token']
 
   if (token) {
     try {
-      return await jwt.verify(token, process.env.SECRET);
+      return await jwt.verify(token, process.env.SECRET)
     } catch (e) {
-      throw new AuthenticationError(
-        'Your session expired. Sign in again.',
-      );
+      throw new AuthenticationError('Your session expired. Sign in again.')
     }
   }
-};
+}
 
 const server = new ApolloServer({
+  introspection: true,
+  playground: true,
   typeDefs: schema,
   resolvers,
   formatError: error => {
@@ -40,60 +37,56 @@ const server = new ApolloServer({
     // leave only the important validation error
     const message = error.message
       .replace('SequelizeValidationError: ', '')
-      .replace('Validation error: ', '');
+      .replace('Validation error: ', '')
 
     return {
       ...error,
       message,
-    };
+    }
   },
   context: async ({ req, connection }) => {
     if (connection) {
       return {
         models,
         loaders: {
-          user: new DataLoader(keys =>
-            loaders.user.batchUsers(keys, models),
-          ),
+          user: new DataLoader(keys => loaders.user.batchUsers(keys, models)),
         },
-      };
+      }
     }
 
     if (req) {
-      const me = await getMe(req);
+      const me = await getMe(req)
 
       return {
         models,
         me,
         secret: process.env.SECRET,
         loaders: {
-          user: new DataLoader(keys =>
-            loaders.user.batchUsers(keys, models),
-          ),
+          user: new DataLoader(keys => loaders.user.batchUsers(keys, models)),
         },
-      };
+      }
     }
   },
-});
+})
 
-server.applyMiddleware({ app, path: '/graphql' });
+server.applyMiddleware({ app, path: '/graphql' })
 
-const httpServer = http.createServer(app);
-server.installSubscriptionHandlers(httpServer);
+const httpServer = http.createServer(app)
+server.installSubscriptionHandlers(httpServer)
 
-const isTest = !!process.env.TEST_DATABASE;
-const isProduction = !!process.env.DATABASE_URL;
-const port = process.env.PORT || 8000;
+const isTest = !!process.env.TEST_DATABASE
+const isProduction = !!process.env.DATABASE_URL
+const port = process.env.PORT || 8000
 
 sequelize.sync({ force: isTest || isProduction }).then(async () => {
   if (isTest || isProduction) {
-    createUsersWithMessages(new Date());
+    createUsersWithMessages(new Date())
   }
 
   httpServer.listen({ port }, () => {
-    console.log(`Apollo Server on http://localhost:${port}/graphql`);
-  });
-});
+    console.log(`Apollo Server on http://localhost:${port}/graphql`)
+  })
+})
 
 const createUsersWithMessages = async date => {
   await models.User.create(
@@ -112,7 +105,7 @@ const createUsersWithMessages = async date => {
     {
       include: [models.Message],
     },
-  );
+  )
 
   await models.User.create(
     {
@@ -133,5 +126,5 @@ const createUsersWithMessages = async date => {
     {
       include: [models.Message],
     },
-  );
-};
+  )
+}
