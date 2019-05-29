f157e062b67fb812b341d2b9c11bd60ead8db1f3
chris023 <chris.meyer023@gmail.com>
2019-05-29 15:18:48 -0600

    Broken since adding error handling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import 'dotenv/config'
+
 import cors from 'cors'
 import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
 
 import schema from './schema'
 import resolvers from './resolvers'
-import models from './models'
+import models, { sequelize } from './models'
 
 const app = express()
 
@@ -13,14 +15,62 @@ app.use(cors())
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
-  context: {
-    models,
-    me: models.users[1],
+  formatError: error => {
+    const message = error.message
+      .replace('SequelizeValidationError: ', '')
+      .replace('Validation error: ', '')
+    return { ...error, message }
   },
+  context: async () => ({
+    models,
+    me: await models.User.findByLogin('rwieruch'),
+  }),
 })
 
 server.applyMiddleware({ app, path: '/graphql' })
 
-app.listen({ port: 8000 }, () => {
-  console.log('Apollo Server on http://localhost:8000/graphql')
+const eraseDatabaseOnSync = true
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  if (eraseDatabaseOnSync) {
+    createUsersWithMessages()
+  }
 })
+
+sequelize.sync().then(async () => {
+  app.listen({ port: 8000 }, () => {
+    console.log('Apollo Server on http://localhost:8000/graphql')
+  })
+})
+
+const createUsersWithMessages = async () => {
+  await models.User.create(
+    {
+      username: 'rwieruch',
+      messages: [
+        {
+          text: 'Published the Road to learn React',
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  )
+
+  await models.User.create(
+    {
+      username: 'ddavids',
+      messages: [
+        {
+          text: 'Happy to release ...',
+        },
+        {
+          text: 'Published a complete ...',
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  )
+}
