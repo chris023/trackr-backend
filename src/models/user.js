fd214dc56ecea3f80d4f3380aecf6437a6135149
chris023 <chris.meyer023@gmail.com>
2019-07-24 12:54:40 -0600

    Add S3FileUpload association

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt'
+import s3fileupload from './s3fileupload'
 
 const user = (sequelize, DataTypes) => {
   const User = sequelize.define('user', {
@@ -30,40 +31,44 @@ const user = (sequelize, DataTypes) => {
     role: {
       type: DataTypes.STRING,
     },
-  });
+  })
 
   User.associate = models => {
-    User.hasMany(models.Message, { onDelete: 'CASCADE' });
-  };
+    User.hasMany(models.Message, { onDelete: 'CASCADE' })
+  }
+
+  User.associate = models => {
+    User.hasMany(models.S3FileUpload, { onDelete: 'CASCADE' })
+  }
 
   User.findByLogin = async login => {
     let user = await User.findOne({
       where: { username: login },
-    });
+    })
 
     if (!user) {
       user = await User.findOne({
         where: { email: login },
-      });
+      })
     }
 
-    return user;
-  };
+    return user
+  }
 
   User.beforeCreate(async user => {
-    user.password = await user.generatePasswordHash();
-  });
+    user.password = await user.generatePasswordHash()
+  })
 
   User.prototype.generatePasswordHash = async function() {
-    const saltRounds = 10;
-    return await bcrypt.hash(this.password, saltRounds);
-  };
+    const saltRounds = 10
+    return await bcrypt.hash(this.password, saltRounds)
+  }
 
   User.prototype.validatePassword = async function(password) {
-    return await bcrypt.compare(password, this.password);
-  };
+    return await bcrypt.compare(password, this.password)
+  }
 
-  return User;
-};
+  return User
+}
 
-export default user;
+export default user
