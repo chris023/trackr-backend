fa1758aef4e02e6c33f32a003720f5a9adadb5d6
chris023 <chris.meyer023@gmail.com>
2019-07-24 13:16:29 -0600

    Remove messages

--- src/seeds/Users.js
+++ src/seeds/Users.js
@@ -1,42 +1,16 @@
-const createUsersWithMessages = async (date, models) => {
-  await models.User.create(
-    {
-      username: 'rwieruch',
-      email: 'hello@robin.com',
-      password: 'rwieruch',
-      role: 'ADMIN',
-      messages: [
-        {
-          text: 'Published the Road to learn React',
-          createdAt: date.setSeconds(date.getSeconds() + 1),
-        },
-      ],
-    },
-    {
-      include: [models.Message],
-    },
-  )
+const createUsersWithMessages = async (models, date) => {
+  await models.User.create({
+    username: 'test123',
+    email: 'test123@tracker.com',
+    password: 'testtest',
+    role: 'ADMIN',
+  })
 
-  await models.User.create(
-    {
-      username: 'ddavids',
-      email: 'hello@david.com',
-      password: 'ddavids',
-      messages: [
-        {
-          text: 'Happy to release ...',
-          createdAt: date.setSeconds(date.getSeconds() + 1),
-        },
-        {
-          text: 'Published a complete ...',
-          createdAt: date.setSeconds(date.getSeconds() + 1),
-        },
-      ],
-    },
-    {
-      include: [models.Message],
-    },
-  )
+  await models.User.create({
+    username: 'theo123',
+    email: 'theo123@gmail.com',
+    password: 'theotheo',
+  })
 }
 
 export default createUsersWithMessages
